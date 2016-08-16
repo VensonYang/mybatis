392e60f456523738605db6bf91a8a1cfdee9bd85
VensonYang <744026144@qq.com>
2016-08-16 17:58:11 +0800

    代码生成模块

---
+++ mybatis/src/main/java/mybatis/plugs/GeneratorAdapter.java
@@ -0,0 +1,15 @@
+package mybatis.plugs;
+
+import java.util.List;
+
+import org.mybatis.generator.api.PluginAdapter;
+
+public class GeneratorAdapter extends PluginAdapter {
+
+	@Override
+	public boolean validate(List<String> arg0) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
